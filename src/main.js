02b65c7ffd8217a16f2b34e3c6e70cc468dbc0fb
jrequejobs <jrequejobs@gmail.com>
2023-12-05 17:43:08 +0100

    add Users view and axios

--- src/main.js
+++ src/main.js
@@ -1,5 +1,20 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import { createRouter, createWebHistory } from 'vue-router'
+import Users from './views/Users.vue'
 
-createApp(App).mount('#app')
+const routes = [
+  { path: '/', component: App },
+  { path: '/users', component: Users } 
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+})
+
+const app = createApp(App)
+app.use(router)
+
+app.mount('#app')
